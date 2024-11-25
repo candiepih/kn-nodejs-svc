8bef202264260cb37962d5c831743b940d78aa02
Alex Njagi <44834632+candiepih@users.noreply.github.com>
2024-11-25 20:25:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const server = require("http").createServer(app);
 const port = process.env.PORT || "8080";
 
 app.get("/", (req, res) => {
-  const name = req.query.name ?? "World!!"
+  const name = req.query.name ?? "World!!!"
   return res.send(`Hello ${name}. Test pipeline that uses buildpacks`);
 });
 server.listen(port, function () {
