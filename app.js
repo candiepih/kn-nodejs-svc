7eb3e4adb621bc59801c8f629a0b533cb9ec1530
Alex Njagi <44834632+candiepih@users.noreply.github.com>
2024-11-28 15:49:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const target = process.env.TARGET;
 
 app.get("/", (req, res) => {
   const targetName = target ?? req.query.name;
-  const name = targetName ?? " World Bana!!"
+  const name = targetName ?? " World!!"
   return res.send(`Hello ${name}. Built with buildpacks`);
 });
 server.listen(port, function () {
